619af756d0761fa09631bd6f2e586ba47c99ea58
PrinceAFelix <princefelix23@icloud.com>
2025-04-14 17:55:53 -0400

    Update terraform config in root main

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,4 +1,21 @@
+terraform {
+  required_version = ">= 1.1.0"
 
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.94.0"
+    }
+  }
+
+}
+
+# Define providers and their config params
+provider "azurerm" {
+  # Leave the features block empty to accept all defaults
+  features {}
+}
 
 resource "azurerm_resource_group" "cst8918_rg" {
   name     = "cst8918-final-project-group-4"
