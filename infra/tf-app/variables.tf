e8040e4526a6a8ecca0377e8115412aab5107519
PrinceAFelix <princefelix23@icloud.com>
2025-04-15 00:45:54 -0400

    chore: fix terraform formatting

--- infra/tf-app/variables.tf
+++ infra/tf-app/variables.tf
@@ -12,11 +12,11 @@ variable "region" {
 variable "resource_group_name" {
   type        = string
   description = "Resource group for main infrastructure"
-  default = "cst8918-final-project-group-num-4"
+  default     = "cst8918-final-project-group-num-4"
 }
 
 variable "subscription_id" {
   description = "The Azure subscription to use for resources"
-  default = "4bd4f7fa-3cb3-4ee9-80fd-102513c31a0f"
+  default     = "4bd4f7fa-3cb3-4ee9-80fd-102513c31a0f"
   type        = string
 }
